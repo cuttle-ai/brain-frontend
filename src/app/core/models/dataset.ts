8ea7a063b816b5374f9145ad8669d997329ea83a
Melvin Davis <melvinodsa@gmail.com>
2020-05-05 00:31:27 +0530

    Line chart integrated

--- src/app/core/models/dataset.ts
+++ src/app/core/models/dataset.ts
@@ -2,44 +2,44 @@
  * Dataset should implemented by any object that has the dataset information
  */
 export interface Dataset {
-    /**
-     * ID is the unique id of the user
-     */
-    ID: number;
-    /**
-     * CreatedAt is the creation date
-     */
-    CreatedAt: Date;
-    /**
-     * UpdatedAt is the updated at date
-     */
-    UpdatedAt: Date;
-    /**
-     * DeletedAt is the deleted at date
-     */
-    DeletedAt: Date;
-    /**
-     * Name of the file
-     */
-    Name: string;
-    /**
-     * Description of the dataset
-     */
-    Description: string;
-    /**
-     * UserID is the id of the user who uploaded the file
-     */
-    UserID: string;
-    /**
-     * ResourceID is the id of the source associated with
-     */
-    ResourceID: number;
-    /**
-     * Source is the type of the dataset source
-     */
-    Source: string;
-    /**
-     * UploadedDataset has the more info about the dataset source
-     */
-    UploadedDataset: any;
+  /**
+   * ID is the unique id of the user
+   */
+  ID?: number;
+  /**
+   * CreatedAt is the creation date
+   */
+  CreatedAt?: Date;
+  /**
+   * UpdatedAt is the updated at date
+   */
+  UpdatedAt?: Date;
+  /**
+   * DeletedAt is the deleted at date
+   */
+  DeletedAt?: Date;
+  /**
+   * Name of the file
+   */
+  Name?: string;
+  /**
+   * Description of the dataset
+   */
+  Description?: string;
+  /**
+   * UserID is the id of the user who uploaded the file
+   */
+  UserID?: string;
+  /**
+   * ResourceID is the id of the source associated with
+   */
+  ResourceID?: number;
+  /**
+   * Source is the type of the dataset source
+   */
+  Source?: string;
+  /**
+   * UploadedDataset has the more info about the dataset source
+   */
+  UploadedDataset?: any;
 }
--- src/app/pages/data/datasets/dataset/dataset-dialog/dataset-dialog.component.ts
+++ src/app/pages/data/datasets/dataset/dataset-dialog/dataset-dialog.component.ts
@@ -1,95 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-import _get from 'lodash/get';
+import { Component, OnInit } from "@angular/core";
+import { get as _get } from "lodash";
 
-import { Dataset } from 'src/app/core/models';
-import { HttpService } from 'src/app/core/services';
-import { NbDialogRef } from '@nebular/theme';
+import { Dataset } from "src/app/core/models";
+import { HttpService } from "src/app/core/services";
+import { NbDialogRef } from "@nebular/theme";
 
 @Component({
-    selector: 'brain-dataset-dialog',
-    templateUrl: './dataset-dialog.component.html',
-    styleUrls: ['./dataset-dialog.component.scss']
+  selector: "brain-dataset-dialog",
+  templateUrl: "./dataset-dialog.component.html",
+  styleUrls: ["./dataset-dialog.component.scss"],
 })
 export class DatasetDialogComponent implements OnInit {
+  /**
+   * dataset being edited
+   */
+  dataset: Dataset;
 
-    /**
-     * dataset being edited
-     */
-    dataset: Dataset;
+  /**
+   * disableUpdateBtn to disable the update button while network request
+   */
+  disableUpdateBtn: boolean = false;
 
-    /**
-     * disableUpdateBtn to disable the update button while network request
-     */
-    disableUpdateBtn: boolean = false;
+  /**
+   * _get is the lodash get function
+   */
+  _get: any;
 
-    /**
-     * _get is the lodash get function
-     */
-    _get: any;
+  /**
+   * error is the error happened while updatingt he form
+   */
+  error: string;
 
-    /**
-     * error is the error happened while updatingt he form
-     */
-    error: string;
+  /**
+   * loading indicates that there is a network request going on
+   */
+  loading: boolean;
 
-    /**
-     * loading indicates that there is a network request going on
-     */
-    loading: boolean
+  /**
+   * forDelete indicates that dialog is for deleting the dataset confirmation message
+   */
+  forDelete: boolean;
 
-    /**
-     * forDelete indicates that dialog is for deleting the dataset confirmation message
-     */
-    forDelete: boolean;
+  /**
+   * We will do the necessary initialisations required by this component
+   */
+  constructor(
+    protected dialogRef: NbDialogRef<DatasetDialogComponent>,
+    private http: HttpService
+  ) {
+    this._get = _get;
+    this.dataset = _get(dialogRef, ["componentRef", "instance", "dataset"], {});
+    this.forDelete = _get(
+      dialogRef,
+      ["componentRef", "instance", "forDelete"],
+      false
+    );
+  }
 
-    /**
-     * We will do the necessary initialisations required by this component
-     */
-    constructor(protected dialogRef: NbDialogRef<DatasetDialogComponent>, private http: HttpService, ) {
-        this._get = _get;
-        this.dataset = _get(dialogRef, ['componentRef', 'instance', 'dataset'], {});
-        this.forDelete = _get(dialogRef, ['componentRef', 'instance', 'forDelete'], false);
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-    }
+  /**
+   * update is called when user clicks on the update button
+   */
+  update() {
+    this.loading = true;
+    this.http
+      .post({
+        hash: "DATASET_UPDATE",
+        body: this.dataset,
+      })
+      .subscribe((resp) => {
+        this.loading = false;
+        this.error = _get(resp, ["error", "error"]);
+        if (!this.error) {
+          this.dialogRef.close();
+        }
+      });
+  }
 
-    /**
-     * update is called when user clicks on the update button
-     */
-    update() {
-        this.loading = true;
-        this.http.post({
-            hash: 'DATASET_UPDATE', body: this.dataset,
-        }).subscribe((resp) => {
-            this.loading = false;
-            this.error = _get(resp, ['error', 'error']);
-            if (!this.error) {
-                this.dialogRef.close();
-            }
-        })
-    }
+  /**
+   * cancel is invoked when user clicks on the cancel button
+   */
+  cancel() {
+    this.dialogRef.close(true);
+  }
 
-    /**
-     * cancel is invoked when user clicks on the cancel button
-     */
-    cancel() {
-        this.dialogRef.close(true);
-    }
-
-    /**
-     * confirmDelete is invoked when the user confoirms to delete the dataset
-     */
-    confirmDelete() {
-        this.loading = true;
-        this.http.post({
-            hash: 'DATASET_DELETE', body: this.dataset,
-        }).subscribe((resp) => {
-            this.loading = false;
-            this.error = _get(resp, ['error', 'error']);
-            if (!this.error) {
-                this.dialogRef.close();
-            }
-        })
-    }
+  /**
+   * confirmDelete is invoked when the user confoirms to delete the dataset
+   */
+  confirmDelete() {
+    this.loading = true;
+    this.http
+      .post({
+        hash: "DATASET_DELETE",
+        body: this.dataset,
+      })
+      .subscribe((resp) => {
+        this.loading = false;
+        this.error = _get(resp, ["error", "error"]);
+        if (!this.error) {
+          this.dialogRef.close();
+        }
+      });
+  }
 }
